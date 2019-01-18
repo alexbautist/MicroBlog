d2fffb8d3ca07eb6954a6494a3c886a69410785e
alejandrobr <alejandro.reyes@alumno.uttehuacan.edu.mx>
2019-01-18 19:47:13 +0100

    Corrections génerales

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ $().on('click', '.page-item', );
 
 });
 
+
+//  Fonction pour voter avec ajax.
 function voter() {
     var idVotes= $(this).parentsUntil('blockquote').find('u').attr('name');
     console.log(idVotes);
@@ -13,45 +15,46 @@ function voter() {
         data: data,
         url: 'Votes.php',                       
         type: 'Post',
-        success: function (responseText) {
-           
+        success: function (responseText) {          
            if(responseText==="Error"){
               $('button[name='+idVotes+']').addClass("disabled");              
            }
            else{
              $('u[name='+idVotes+']').text(responseText);
-         }
+               }
         },
         error: function () {
         }
     });
 }
 
+// Fonction pour insérer les commentaires 
 function insererCommentaire(comment) {
     var parametros = {'contenu': comment};
     $.ajax({
-        data: parametros, //aqui tus datos
-        url: 'modifierSupprimerCommentaire.php', //aqui va tu direccion donde esta tu funcion php                        
-        type: 'post', //aqui puede ser igual get
+        data: parametros,
+        url: 'modifierSupprimerCommentaire.php',                       
+        type: 'post',
         success: function (response) {
         },
         error: function (data) {
-            //lo que devuelve si falla tu archivo mifuncion.php
         }
     });
 }
+
+// Fonction pour modifier les commentaires 
 function modifierCommentaire(comment, id) {
     console.log("llega");
     var parametros = {'contenu': comment, 'id': id};
     $.ajax({
-        data: parametros, //aqui tus datos
-        url: 'modifierCommentaire.php', //aqui va tu direccion donde esta tu funcion php                        
-        type: 'post', //aqui puede ser igual get
+        data: parametros,
+        url: 'modifierCommentaire.php',                      
+        type: 'post',
         success: function (response) {
            location.href = "index.php";
         },
         error: function (data) {
-            alert("no funciona"); //lo que devuelve si falla tu archivo mifuncion.php
+            alert("Erreur");
         }
     });
 }  
